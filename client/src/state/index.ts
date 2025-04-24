95de1f34550843938af552d01e31710e833c1ad8
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-04-24 13:00:57 +0100

    redux

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -14,12 +14,14 @@ export const globalSlice = createSlice({
   name: "global",
   initialState,
   reducers: {
-    setIsSidebarCollapsed: (state, action: PayloadAction) => {},
-    // setGlobalState: (state, action) => {
-    //   return { ...state, ...action.payload };
-    // },
+    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSidebarCollapsed = action.payload;
+    },
+    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    },
   },
 });
 
-export const {} = globalSlice.actions;
+export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
 export default globalSlice.reducer;
